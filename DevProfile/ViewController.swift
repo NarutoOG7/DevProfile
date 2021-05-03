da28bb2d8acf36e0732fe55bd443b6ed9cb3f169
Spencer Belton <23slowmojo@gmail.com>
2021-05-02 20:50:28 -0600

    initial commit, I believe

---
+++ DevProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by Spencer Belton on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
